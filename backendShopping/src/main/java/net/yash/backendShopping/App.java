e355eaa196693906ac0cc02fec90b14b9cc17049
yash <vyashwanth01@gmail.com>
2018-12-24 11:09:57 -0800

    full stack java project setup completed sucessfully !!

---
+++ backendShopping/src/main/java/net/yash/backendShopping/App.java
@@ -0,0 +1,13 @@
+package net.yash.backendShopping;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
